e339a1d8a87922f4e95070df984321f7707e6c02
juan.campos <juan.campos@stgen.com>
2024-11-27 16:15:00 -0500

    add navigation

--- App.tsx
+++ App.tsx
@@ -1,9 +1,21 @@
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
 import { Categories } from "./screens/Categories";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
 
+const Stack = createNativeStackNavigator();
 export default function App() {
-  return <Categories />;
+  return (
+    <>
+      <StatusBar style="light" />
+      <NavigationContainer>
+        <Stack.Navigator>
+          <Stack.Screen name="Meals Categories" component={Categories} />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </>
+  );
 }
 
 const styles = StyleSheet.create({
